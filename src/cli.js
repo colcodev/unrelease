6c86329e892215217b6ceb4124a0f5056d928c53
COLCO <mario@hashdog.com>
2020-10-07 19:54:34 -0300

    added pull-request info and new line to add

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,30 @@
 import { Octokit } from '@octokit/rest';
 
-const octokit = new Octokit();
-
-octokit.pulls
-  .list({
-    owner: 'reactjs',
-    repo: 'reactjs.org',
-    state: 'open',
-  })
-  .then(({ data }) => {
-    console.log('data', data);
-  });
-
 export function cli(args) {
-  console.log('cli -> args', args);
+  console.log('args', args);
+
+  const octokit = new Octokit();
+
+  const owner = 'reactjs';
+  const repo = 'reactjs.org';
+
+  octokit.pulls
+    .list({
+      owner,
+      repo,
+      state: 'open',
+    })
+    .then(({ data }) => {
+      console.log('data.length', data.length);
+      return octokit.pulls.get({
+        owner,
+        repo,
+        pull_number: 3181,
+      });
+    })
+    .then(({ data }) => {
+      const { number, title, user } = data;
+      const str = `- [${number}] ${title} @${user.login}`;
+      console.log(str);
+    });
 }
